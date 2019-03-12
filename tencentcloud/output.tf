0f5534ec91d1d004af82fa971d122fb1a01d25b1
test <test@test.com>
2019-03-12 23:40:34 +0800

    Add 1st tencent terraform demo

---
+++ tencentcloud/output.tf
@@ -0,0 +1,19 @@
+output "vpc_a_id" {
+  value = "${tencentcloud_vpc.vpc_a.id}"
+}
+
+output "vm_instance_id" {
+  value = "${tencentcloud_instance.vm.*.id}"
+}
+
+output "vm_private_id" {
+  value = "${tencentcloud_instance.vm.*.private_ip}"
+}
+
+output "vm_public_id" {
+  value = "${tencentcloud_instance.vm.*.public_ip}"
+}
+
+output "vm_image_id" {
+  value = "${tencentcloud_instance.vm.*.image_id}"
+}
